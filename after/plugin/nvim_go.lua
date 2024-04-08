1cd1161e86a548b83ba548293006c831400752c5
reedjosh <josh@thereedfamily.rocks>
2024-04-08 10:57:02 -0500

    Updated for deprecated goimport var.

--- after/plugin/nvim_go.lua
+++ after/plugin/nvim_go.lua
@@ -1,5 +1,5 @@
 require 'go'.setup({
-    goimport = 'gopls', -- if set to 'gopls' will use golsp format
+    goimports = 'gopls', -- if set to 'gopls' will use golsp format
     gofmt = 'gopls', -- if set to gopls will use golsp format
     max_line_len = 120,
     tag_transform = false,
